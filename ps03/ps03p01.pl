92b62bacdf78d4d134154c97d4451afdaaddc100
Low-Wee Heng <alpha116@gmail.com>
2011-09-17 10:22:09 +0800

    commit

--- ps03/ps03p01.pl
+++ ps03/ps03p01.pl
@@ -3,4 +3,45 @@ Heng Low Wee
 U096901R
 
 Problem Set 3 Problem 1
+
+beside(Image1, Image2)
+side by side, horizontal 50%
+
+rotate(Image)
+rotate 90º clockwise
+
+scale(Image)
+scale by 50% horizontal only, vertical 100%
+
+All input/resulting images are 640x640
+
 */
+
+%:- op(precedence, type, name).
+%:- op(precedence, type, name).
+
+% whole string of expression
+montage(Expr, File).
+
+% beside takes in double arguments
+beside(Arg1, Arg2).
+
+beside(Arg1, Arg1) :-
+	write('convert +append '),
+	write(Arg1),
+	write(' '),
+	write(Arg2),
+	write(' '),
+	writeln(Output),
+	!.
+
+% rotate takes in single argument
+rotate(Arg).
+
+rotate(Arg) :-
+	write('convert -rotate 90 '),
+	write(Input),
+	write(' '),
+	writeln(Output),
+	!.
+	
