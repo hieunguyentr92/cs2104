ebdad7369e37e3b46aadf32dcd9714f5338df323
Live System User <liveuser@localhost.localdomain>
2011-09-19 14:14:37 -0400

    ps03p01 looks correct

--- ps03/ps03p01.pl
+++ ps03/ps03p01.pl
@@ -25,7 +25,7 @@ montage(Expr, File) :- Expr =.. [Op, Arg1, Arg2],
 	montage(Arg2, F2),
 	(Op = beside
 		-> atom_concat(F1, '1', F11), write('convert -scale 50%%x100%% '), write(F1), write('.jpg '), write(F11), write('.jpg '), writeln(''), 
-		   atom_concat(F2, '1', F22), write('convert -scale 50%%x100%% '), write(F2), write('.jpg '), write(F22), write('.jpg '), writeln(''),
+		   atom_concat(F2, '2', F22), write('convert -scale 50%%x100%% '), write(F2), write('.jpg '), write(F22), write('.jpg '), writeln(''),
 		   atom_concat(F11, F22, Fout), write('convert +append '), write(F11), write('.jpg '), write(F22), write('.jpg '), write(Fout), write('.jpg '), writeln('')
 	),
 	File = Fout,
@@ -34,7 +34,7 @@ montage(Expr, File) :- Expr =.. [Op, Arg1, Arg2],
 montage(Expr, File) :- Expr =.. [Op, Arg1],
 	montage(Arg1, F1),
 	(Op = rotate
-		-> atom_concat(F1, '1', R),
+		-> atom_concat(F1, '3', R),
 write('convert -rotate 90 '), write(F1), write('.jpg '), write(R), write('.jpg '), writeln('')
 	),
 	File = R,
