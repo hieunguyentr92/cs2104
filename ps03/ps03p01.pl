5090d8d1e95a8acfba3d5b0b6ae1a6706350487a
Live System User <liveuser@localhost.localdomain>
2011-09-19 14:01:57 -0400

    ps03p01 should be ok

--- ps03/ps03p01.pl
+++ ps03/ps03p01.pl
@@ -24,17 +24,20 @@ montage(Expr, File) :- Expr =.. [Op, Arg1, Arg2],
 	montage(Arg1, F1),
 	montage(Arg2, F2),
 	(Op = beside
-		-> write('convert -scale 50%%x50%% '), write(F1), write('.jpg '), write(F1), write('.jpg '), writeln(''), 
-		   write('convert -scale 50%%x50%% '), write(F1), write('.jpg '), write(F1), write('.jpg '), writeln(''), 
-		   write('convert +append file1 file2 outfile'), writeln('')
+		-> atom_concat(F1, '1', F11), write('convert -scale 50%%x100%% '), write(F1), write('.jpg '), write(F11), write('.jpg '), writeln(''), 
+		   atom_concat(F2, '1', F22), write('convert -scale 50%%x100%% '), write(F2), write('.jpg '), write(F22), write('.jpg '), writeln(''),
+		   atom_concat(F11, F22, Fout), write('convert +append '), write(F11), write('.jpg '), write(F22), write('.jpg '), write(Fout), write('.jpg '), writeln('')
 	),
+	File = Fout,
 	!.
 	
 montage(Expr, File) :- Expr =.. [Op, Arg1],
 	montage(Arg1, F1),
 	(Op = rotate
-		-> write('convert -rotate 90 '), write('input '), write('.jpg '), write('output'), write('.jpg '), writeln('')
+		-> atom_concat(F1, '1', R),
+write('convert -rotate 90 '), write(F1), write('.jpg '), write(R), write('.jpg '), writeln('')
 	),
+	File = R,
 	!.
 
 % no command, just atom
