5d49215b380bddaf9b8b0b117785b87e9e849084
Low-Wee Heng <alpha116@gmail.com>
2011-09-29 14:43:42 +0800

    minor changes

--- tut5/ex1.py
+++ tut5/ex1.py
@@ -4,10 +4,10 @@
 
 mylist = [123, 234, 345, 456]
 
-def sum(list1):
+def sumitup(list1):
 	temp = 0
 	for num in list1:
 		temp += num
 	print 'Total is %d' % (temp)
 	
-sum(mylist)
+sumitup(mylist)
