23aa42e51e5f3ca4a96c512b593fb7baa4ae8514
Low-Wee Heng <alpha116@gmail.com>
2011-10-01 16:33:35 +0800

    setting up the turtle

---
+++ ps04/output.py
@@ -0,0 +1,4 @@
+from turtle import *
+import time
+_G570
+time.sleep(30)
--- ps04/ps04ps01.pl
+++ ps04/ps04ps01.pl
@@ -1,1 +1,15 @@
-fractal(Equation, Int, Length) :- true.
+/*HENG LOW WEE
+U096901R
+Problem Set 4 Problem 1*/
+
+fractal(X=Expr, K, Length) :- 
+	writeln(Expr),
+	!.
+	
+:- tell('output.py'),
+	writeln('from turtle import *'),
+	writeln('import time'),
+ 	fractal(X=Expr, K, Length),
+	writeln('time.sleep(30)'),
+	told,
+	!.
