5213f6c4b1c73f810c8f0d3169aa7332bbf9fccd
Low-Wee Heng <alpha116@gmail.com>
2011-10-01 19:59:55 +0800

    ps04p01 ugly, but working

--- ps04/ps04ps01.pl
+++ ps04/ps04ps01.pl
@@ -15,46 +15,44 @@ Test Case : fractal(g = (g;left(45);g;right(90);g;left(45);g),3,30).
 
 */
 
+fractal(_=_, 0, L) :-
+	write('forward('), write(L), writeln(')'),
+	!.
+
 fractal(X=(Expr),K,L) :-
-	writeln('from turtle import *'),
-	writeln('import time'),
-	writeln('Screen().setworldcoordinates(0,-500, 1500, 1000)'),
-	writeln('delay(0)'),
-	fractal(X, Expr, K, L),	
-	!.
-
-fractal(X, X, 0, L) :-
-	write('forward('),
-	write(L),
-	writeln(')'),
+	fractal(X, Expr, Expr, K, L),
 	!.
 	
-fractal(X, X, K, L) :-
-	writeln('hello'),
+fractal(X, X, _, 1, L) :-
+	write('forward('), write(L), writeln(')'),
+	!.
+	
+fractal(X, X, Expr, K, L) :-
 	K1 is K-1,
-	fractal(X, X, K1, L),
+	L1 is L*0.85^(K1),
+	fractal(X, Expr, Expr, K1, L1),
 	!.
 	
-fractal(_, left(Angle), _, _) :-
-	write('left('),
-	write(Angle),
-	writeln(')'),
+fractal(_, left(Angle), _, _, _) :-
+	write('left('), write(Angle), writeln(')'),
 	!.
 
-fractal(_, right(Angle), _, _) :-
-	write('right('),
-	write(Angle),
-	writeln(')'),
+fractal(_, right(Angle), _, _, _) :-
+	write('right('), write(Angle), writeln(')'),
 	!.
 	
-fractal(X, Left;Right, K, L) :-
-	fractal(X, Left, K, L),
-	fractal(X, Right, K, L),
+fractal(X, Left;Right, Expr, K, L) :-
+	fractal(X, Left, Expr, K, L),
+	fractal(X, Right, Expr, K, L),
 	!.
 	
 :- tell('output.py'),
+	writeln('from turtle import *'),
+	writeln('import time'),
+	writeln('Screen().setworldcoordinates(-200,-500, 1500, 1000)'),
+	writeln('delay(0)'),
 	% -----> ENTER EQUATION HERE
- 	fractal(g = (g;left(45);g),2,30),
+ 	fractal(g=(left(120);g;right(60);g;right(60);g;right(60);g;g;left(60)),4,30),
 	% After moving so much, the turtle is tired and has fallen asleep
 	writeln('time.sleep(10)'),
 	told,
