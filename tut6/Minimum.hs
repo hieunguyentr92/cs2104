8f89249b1dfe1c738f381afd94c4754af94ef58c
Low-Wee Heng <alpha116@gmail.com>
2011-10-05 20:55:03 +0800

    finally some progress

--- tut6/Minimum.hs
+++ tut6/Minimum.hs
@@ -2,7 +2,7 @@
 --U096901R
 --CS2104 Tut 6 Ex 1
 
-getmin [] = print "empty"
-getmin (h:t) 
-	| h==1	= print "equals 1"
-	| otherwise = print "not equals 1"
+getmin [] = error "Form is emptiness; Emptiness is form"
+getmin [x] = x
+getmin [x,y] = min x y
+getmin (h:t) = 0
