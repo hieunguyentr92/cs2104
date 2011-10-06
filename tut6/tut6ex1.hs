454eec4ec63aacc3f63e533ed89c3b7bdbb738dc
Low-Wee Heng <alpha116@gmail.com>
2011-10-06 15:55:07 +0800

    tut6 ex1

--- tut6/tut6ex1.hs
+++ tut6/tut6ex1.hs
@@ -2,7 +2,8 @@
 --U096901R
 --CS2104 Tut 6 Ex 1
 
+getmin :: [Integer] -> Integer
 getmin [] = error "Form is emptiness; Emptiness is form"
 getmin [x] = x
 getmin [x,y] = min x y
-getmin (h:t) = 0 -- need to fix this part
+getmin (h:t) = min h (getmin t)
