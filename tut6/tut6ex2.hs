8553a3f17913ef23f329b8f67d5b89029a951773
Low-Wee Heng <alpha116@gmail.com>
2011-10-06 20:22:58 +0800

    jizz in my pants

--- tut6/tut6ex2.hs
+++ tut6/tut6ex2.hs
@@ -2,9 +2,6 @@
 --U096901R
 --Tut 6 Ex 2
 
-zero = 0
-b = 1
-func :: (Int, Int, Int) -> (Int, Int, Int)
-func (i, a, b)
-	| (a & 1 == 0) = -- a<<=1, b=3*a+1
-	| otherwise = -- a=2*b+1,b--
+
+ex2 a
+	| (i==100) = a
