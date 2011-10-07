1e35e2e7b881b1989d33c40a8f945e14d4f25397
Low-Wee Heng <alpha116@gmail.com>
2011-10-07 14:05:00 +0800

    problem 2

--- ps05/ps05ex02.hs
+++ ps05/ps05ex02.hs
@@ -1,7 +1,6 @@
 --HENG LOW WEE
 --U096901R
 --Problem Set 5 Problem 2
-
 -- count x l = foldr <code> 0 <code> 
 
-count x l = foldr (\x y -> if x==y then x+1 else x) 0 l
+count x l = foldr (\a b -> b+1) 0 (filter(==x) l)
