a32854f5f01276428f7b108758554210f6365f72
Low-Wee Heng <alpha116@gmail.com>
2011-10-07 22:55:42 +0800

    working

--- ps05/ps05ex03.c
+++ ps05/ps05ex03.c
@@ -3,10 +3,31 @@
 // Problem Set 5 Problem 3
 #include <stdio.h>
 
-int main (int argc, char** argv) {
-	return 0;
+// pseudo coding
+
+// function declaration
+float halfInterval(char *f, float x1, float x2, float esp);
+
+// main function
+float main (int argc, char** argv) {
+	char[] f = your function;
+	float x1 = <value>;
+	float x2 = <value>;
+	float esp = <value>;
+	float (*ptr)(char *f, float x1, float x2, float esp);
+	
+	return (*halfInterval)(f,x1,x2,esp);
 }
 
-//double (*(*foo)(char[],float, float, float))
-
-float (*halfInterval)(char[],float,float,float);
+// function definition
+float (*halfInterval)(char *f,float x1,float x2,float esp) {
+	if (absolute(x1-x2) < esp) {
+		return (x1+x2)/2;
+	}
+	else if ((f x1)*(f (x1+x2)/2) <= 0) {
+		return (*halfInterval)(f, x1, ((x1+x2)/2), esp);
+	}
+	else if ((f x2)*(f (x1+x2)/2) <= 0) {
+		return (*halfInterval)(f, ((x1+x2)/2), x2, esp);
+	}
+}
