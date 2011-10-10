ea38a21c50918c2c47ffaa0c15d686043a873d35
Low-Wee Heng <alpha116@gmail.com>
2011-10-10 12:52:58 +0800

    ps5ps03

--- ps05/ps05ex03.c
+++ ps05/ps05ex03.c
@@ -2,32 +2,35 @@
 // U096901R
 // Problem Set 5 Problem 3
 #include <stdio.h>
-
-// pseudo coding
+#include <stdlib.h>
+#include <math.h>
 
 // function declaration
-float halfInterval(char *f, float x1, float x2, float esp);
+double halfInterval(double (*f)(double), double x1, double x2, double esp);
+
+
+double g_x(double x) {
+	return x*x;
+}
 
 // main function
-float main (int argc, char** argv) {
-	char[] f = your function;
-	float x1 = <value>;
-	float x2 = <value>;
-	float esp = <value>;
-	float (*ptr)(char *f, float x1, float x2, float esp);
-	
-	return (*halfInterval)(f,x1,x2,esp);
+int main (int argc, char** argv) {	
+	printf("%f\n",halfInterval(&sin, 1.0, 4.0, 0.000000000000001));
+	printf("%f\n",halfInterval(&cos, 1.0, 4.0, 0.000000000000001));
+	printf("%f\n",halfInterval(&tan, 1.0, 4.0, 0.000000000000001));
+	printf("%f\n",halfInterval(&g_x, -1.0, 4.0,0.000000000000001));
+	return 0;
 }
 
 // function definition
-float (*halfInterval)(char *f,float x1,float x2,float esp) {
-	if (absolute(x1-x2) < esp) {
+double halfInterval (double (*f)(double),double x1,double x2,double esp) {
+	if (fabs(x1-x2) < esp) {
 		return (x1+x2)/2;
 	}
-	else if ((f x1)*(f (x1+x2)/2) <= 0) {
-		return (*halfInterval)(f, x1, ((x1+x2)/2), esp);
+	else if (f(x1)*f((x1+x2)/2) <= 0) {
+		return halfInterval(f, x1, (x1+x2)/2, esp);
 	}
-	else if ((f x2)*(f (x1+x2)/2) <= 0) {
-		return (*halfInterval)(f, ((x1+x2)/2), x2, esp);
+	else if (f(x2)*f((x1+x2)/2) <= 0) {
+		return halfInterval(f, (x1+x2)/2, x2, esp);
 	}
 }
