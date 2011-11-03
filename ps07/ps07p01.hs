e26c75927a3206f94c145da54a8fb9baa420dfee
Low-Wee Heng <alpha116@gmail.com>
2011-11-03 10:58:38 +0800

    fed up, really

--- ps07/ps07p01.hs
+++ ps07/ps07p01.hs
@@ -118,8 +118,8 @@ nand_gate i1 i2 = not_gate (and_gate i1 i2)
 nand_gate3 :: [Bool] -> [Bool] -> [Bool] -> [Bool]
 nand_gate3 i1 i2 i3 = not_gate (and_gate3 i1 i2 i3)
 
-jk_gate1 :: [Bool] -> [Bool] -> [Bool] -> [Bool]
-jk_gate1 j c k =
+jk_gate2 :: [Bool] -> [Bool] -> [Bool] -> [Bool]
+jk_gate2 j c k =
 	let (q,qbar,w1,w2) =
 		(nand_gate w1 qbar,
 		nand_gate q w2,
@@ -141,11 +141,28 @@ jkflipper True False False q = q
 jkflipper True True True q = not q
 jkflipper True False True q = q
 
-jk_gate :: [Bool] -> [Bool] -> [Bool] -> Bool -> [Bool]
-jk_gate (j:js) (c:cs) (k:ks) q = q:(jk_gate js cs ks (jkflipper j c k q))
+jk_gatehelper :: [Bool] -> [Bool] -> [Bool] -> Bool -> [Bool]
+jk_gatehelper (j:js) (c:cs) (k:ks) q = q:(jk_gatehelper js cs ks (jkflipper j c k q))
+
+jk_gate :: [Bool] -> [Bool] -> [Bool] -> [Bool]
+jk_gate j c k = delay 0 True (delay 1 False (jk_gatehelper j c k False))
+
+--jk_gate1 :: [Bool] -> [Bool] -> [Bool] -> [Bool]
+--jk_gate1 j c k = delay 0 True (delay 1 False (jk_gatehelper j c k False))
 
 jk_zipper :: (a -> b -> c -> d -> e) -> [a] -> [b] -> [c] -> [d] -> [e]
 jk_zipper z (a:as) (b:bs) (c:cs) (d:ds) = z a b c d : jk_zipper z as bs cs ds
 
 jkflipflop :: [Bool] -> [Bool] -> [Bool] -> [(Bool,Bool,Bool,Bool)]
-jkflipflop j c k = let (q1,q2,q3,q4) = (jk_gate j c k False, jk_gate q1 c q1 False, jk_gate (and_gate1 q1 q2) c (and_gate1 q1 q2) False, jk_gate (and_gate1 (and_gate1 q1 q2) q3) c (and_gate1 (and_gate1 q1 q2) q3) False) in jk_zipper (\p q r s -> (s,r,q,p)) q1 q2 q3 q4
+jkflipflop j c k = let (q1,q2,q3,q4) = (delay 10 True (jk_gate j c k), delay 8 True (jk_gate q1 c q1), delay 6 True (jk_gate (and_gate q1 q2) c (and_gate q1 q2)), delay 4 True (jk_gate (and_gate (and_gate q1 q2) q3) c (and_gate (and_gate q1 q2) q3))) in jk_zipper (\p q r s -> (s,r,q,p)) q1 q2 q3 q4
+
+--jkflipflop1 :: [Bool] -> [Bool] -> [Bool] -> [(Bool,Bool,Bool,Bool)]
+--jkflipflop1 j c k = let (q1,q2,q3,q4) = (jk_gate1 j c k, jk_gate1 q1 c q1, jk_gate1 (and_gate1 q1 q2) c (and_gate1 q1 q2), jk_gate1 (and_gate1 (and_gate1 q1 q2) q3) c (and_gate1 (and_gate1 q1 q2) q3)) in jk_zipper (\p q r s -> (s,r,q,p)) q1 q2 q3 q4
+
+booleanconvert a = if a==True then 1 else 0
+
+tupleconvert (a,b,c,d) = (booleanconvert a, booleanconvert b, booleanconvert c, booleanconvert d)
+
+display j c k = foldl (\x (p,q,r,s) -> x++[tupleconvert (p,q,r,s)]) [] (take 100 (jkflipflop j c k))
+
+display1 j c k = foldl (\x (p,q,r,s) -> x++[tupleconvert (p,q,r,s)]) [] (take 100 (jkflipflop1 j c k))
